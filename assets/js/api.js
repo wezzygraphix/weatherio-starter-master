67d20ef46306f75c4b3a8ad2886eb8a48fe91eb8
Wezzy <chibulawezzy@gmail.com>
2024-11-12 11:31:26 +0200

    Added weather condition

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,41 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict'
+
+const api_key = "05a6515d1f1056de11359fa2d38b2479";
+
+/**
+ * fetch data from server
+ * @param {string} URL API url 
+ * @param {Function} callback 
+ */
+
+export const fetchData = function (URL, callback) {
+    fetch('${URL}&appid=${api_key}')
+        .then(res = res.json())
+        .then(data = callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&{lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /** 
+    * @param {string} query search query e.g: "London,"
+    * @returns
+    */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?${query}&limit=5`
+    }
+}
