b21d6da6bc3ddb3c3199146d609ea8478b79d2f8
Wezzy <chibulawezzy@gmail.com>
2024-11-23 12:49:45 +0200

    Changed the Logo

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,40 +5,39 @@
  * @author wezzy chibula <chibulawezzy33@gmail.com>
  */
 
-'use strict'
+'use strict';
 
-const api_key = "05a6515d1f1056de11359fa2d38b2479";
+const api_key = "bfa2c6ba9d28858722774fc29a57a440";
 
 /**
- * fetch data from server
- * @param {string} URL API url 
- * @param {Function} callback 
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
  */
 
 export const fetchData = function (URL, callback) {
-    fetch('${URL}&appid=${api_key}')
-        .then(res = res.json())
-        .then(data = callback(data));
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
 }
 
 export const url = {
-    currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric` 
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
     },
-    forecast(lat, lon) {
+    forecast(lat, lon){
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
-    /** 
-    * @param {string} query search query e.g: "London,"
-    * @returns
-    */
+    /**
+     * @param {string} query Search query e.g.: "Solwezi", "Lusaka"
+     */
     geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 }
